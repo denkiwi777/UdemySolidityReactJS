7b8bbe642c060b99a7b63154db57d0fe86400dbe
denkiwi777 <64206164+denkiwi777@users.noreply.github.com>
2021-12-18 15:45:35 +0100

    UX
    UX improvements

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -12,20 +12,34 @@ function App() {
 
   const [account, setAccount] = useState(null);
   const [balance, setBalance] = useState(null);
-  const [shouldReload ,reloadAfter] = useState(false);
-  const reloadEffect = useCallback(() => reloadAfter(!shouldReload), [shouldReload])
+  const [shouldReload, reloadAfter] = useState(false);
+  const reloadEffect = useCallback(
+    () => reloadAfter(!shouldReload),
+    [shouldReload]
+  );
 
+  const setAccountListener = (provider) => {
+    provider.on("accountsChanged", (_) => window.location.reload());
+    // provider._jsonRpcConnection.events.on("notification", payload => {
+    //   const { method} = payload;
+    //   if(method === "metamask_unlockStateChanged") {
+    //     setAccount(null);
+    //   }
+
+    // })
+  };
   useEffect(() => {
     const loadProvider = async () => {
       const provider = await detectEthereumProvider();
-      const {contract }= await loadContract("FaucetContract", provider)
 
       if (provider) {
+        const { contract } = await loadContract("FaucetContract", provider);
+        setAccountListener(provider);
         provider.request({ method: "eth_requestAccounts" });
         setWeb3Api({
           web3: new Web3(provider),
           provider,
-          contract
+          contract,
         });
       } else {
         console.log("error");
@@ -33,14 +47,14 @@ function App() {
     };
     loadProvider();
   }, []);
-  useEffect(() =>{
+  useEffect(() => {
     const loadBalance = async () => {
-      const {contract, web3} = web3Api
-      const balance = await web3.eth.getBalance(contract._address)
-      setBalance(web3.utils.fromWei(balance, "ether"))
-    }
-    web3Api.contract && loadBalance()
-  }, [web3Api,shouldReload])
+      const { contract, web3 } = web3Api;
+      const balance = await web3.eth.getBalance(contract._address);
+      setBalance(web3.utils.fromWei(balance, "ether"));
+    };
+    web3Api.contract && loadBalance();
+  }, [web3Api, shouldReload]);
   useEffect(() => {
     const getAccount = async () => {
       const accounts = await web3Api.web3.eth.getAccounts();
@@ -50,35 +64,69 @@ function App() {
   }, [web3Api.web3]);
 
   const addFunds = useCallback(async () => {
-    const { contract, web3 } = web3Api
+    const { contract, web3 } = web3Api;
     await contract.methods.addFunds().send({
       from: account,
-      value: web3.utils.toWei("1", "ether")
+      value: web3.utils.toWei("1", "ether"),
     });
     reloadEffect();
-  }, [web3Api, account, reloadEffect])
+  }, [web3Api, account, reloadEffect]);
 
   const withdraw = async () => {
-    const {contract, web3} = web3Api
-    await  contract.methods.withdraw( web3.utils.toWei("0.1", "ether")).send({from:account} );
-    reloadEffect()
-
-  }
+    const { contract, web3 } = web3Api;
+    await contract.methods
+      .withdraw(web3.utils.toWei("0.1", "ether"))
+      .send({ from: account });
+    reloadEffect();
+  };
   return (
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
           <div className="is-flex is-flex-direction-row">
             <span>
-              <strong className="mr-2"> Account1:</strong>
+              <strong className="mr-2"> Account: </strong>
             </span>
-            {account ? account : "Not connected" }
+            {account ? (
+              <div>{account}</div>
+            ) : !web3Api.provider ? (
+              <>
+              <div className="notification is-warning is-size-7 is-rounded">
+                Wallet is not detected!  
+                <a target="_blank"  rel="noreferrer" href="https://docs.metamask.io">
+                  <strong> Install Metamask</strong>
+                </a>
+
+              </div>
+              </>
+            ) : (
+              <button
+                className="button is-small"
+                onClick={() =>
+                  web3Api.provider.request({ method: "eth_requestAccounts" })
+                }
+              >
+                Connect Wallet
+              </button>
+            )}
           </div>
           <div className="balance-view is-size-2">
             Current Balance <strong>{balance}</strong> ETH
           </div>
-          <button onClick={addFunds} className="button is-primary mr-2">Donate 1 ETH </button>
-          <button onClick = {withdraw} className="button is-link">Withdraw 0.1 ETH</button>
+          <button
+            onClick={addFunds}
+            className="button is-primary mr-2"
+            disabled={!account}
+          >
+            Donate 1 ETH{" "}
+          </button>
+          <button
+            onClick={withdraw}
+            className="button is-link"
+            disabled={!account}
+          >
+            Withdraw 0.1 ETH
+          </button>
         </div>
       </div>
     </>
